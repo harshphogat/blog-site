806c77d7aac814c5cc27b344afc9a5aed2181566
Sunny Kumar Yadav <mailsunny333@gmail.com>
2024-05-20 12:08:51 +0530

    TRYING -1

--- src/main.jsx
+++ src/main.jsx
@@ -84,6 +84,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
+          
             <Route index element={<Home />} />
             {/* <Route 
               path="/"
