2c6d227027ecec9d04f35b76420a6f63627b2997
Harsh Kumar Phogat <55057156+harshphogat@users.noreply.github.com>
2024-05-27 21:54:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -173,9 +173,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
       <BrowserRouter >
         <Routes>
+           <Route path="/" element={<App />} >
           {
             ANDRoutes.map((route, index) => {
-              return  <Route path="/" element={<App />} >
+              return 
 
             <Route index element={<Home /> }/>
 
@@ -220,10 +221,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
               }
             />
             <Route path="post/:slug" element={<Post />} />
-          </Route>
+          
             })
           }
-         
+         </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
